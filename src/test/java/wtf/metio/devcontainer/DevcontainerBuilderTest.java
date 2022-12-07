836936a1c8dc6312b2f1d97139be61307f0ae87c
Sebastian Hoß <seb@ho.de>
2022-12-07 09:45:29 +0100

    extend test case for builders

--- src/test/java/wtf/metio/devcontainer/DevcontainerBuilderTest.java
+++ src/test/java/wtf/metio/devcontainer/DevcontainerBuilderTest.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: The devcontainer.java Authors
+ * SPDX-License-Identifier: 0BSD
+ */
 package wtf.metio.devcontainer;
 
 import org.junit.jupiter.api.Assertions;
@@ -7,9 +11,23 @@ class DevcontainerBuilderTest {
 
   @Test
   void createInstanceWithBuilder() {
-    final var devcontainer = DevcontainerBuilder.builder().name("test").create();
+    final var devcontainer = DevcontainerBuilder.builder()
+        .name("test")
+        .create();
 
     Assertions.assertEquals("test", devcontainer.name());
+  }
+
+  @Test
+  void createNestedBuildComponent() {
+    final var devcontainer = DevcontainerBuilder.builder()
+        .name("test")
+        .build(BuildBuilder.builder().context(".").dockerfile("Containerfile").create())
+        .create();
+
+    Assertions.assertEquals("test", devcontainer.name());
+    Assertions.assertEquals(".", devcontainer.build().context());
+    Assertions.assertEquals("Containerfile", devcontainer.build().dockerfile());
   }
 
 }
